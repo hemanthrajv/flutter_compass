0c049b97f0bde7619bf7d339071d76f705aa576a
Sebastian Roth <sebastian.roth@gmail.com>
2019-04-01 11:02:41 +0800

    Request permissions within the example project only

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,25 +2,27 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  double _direction;
+  bool _hasPermissions = false;
 
   @override
   void initState() {
     super.initState();
-    FlutterCompass.events.listen((double direction) {
-      setState(() {
-        _direction = direction;
-      });
-    });
+
+    _fetchPermissionStatus();
   }
 
   @override
@@ -30,15 +32,85 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Flutter Compass'),
         ),
-        body: Container(
+        body: Builder(builder: (context) {
+          if (_hasPermissions) {
+            return _buildCompass();
+          } else {
+            return _buildPermissionSheet();
+          }
+        }),
+      ),
+    );
+  }
+
+  Widget _buildCompass() {
+    return StreamBuilder<double>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error reading heading: ${snapshot.error}');
+        }
+
+        if (!snapshot.hasData) {
+          return Center(
+            child: Container(
+              width: 32,
+              height: 32,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        double direction = snapshot.data;
+
+        return Container(
           alignment: Alignment.center,
           color: Colors.white,
           child: Transform.rotate(
-            angle: ((_direction ?? 0) * (math.pi / 180) * -1),
+            angle: ((direction ?? 0) * (math.pi / 180) * -1),
             child: Image.asset('assets/compass.jpg'),
           ),
-        ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPermissionSheet() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Location Permission Required'),
+          RaisedButton(
+            child: Text('Request Permissions'),
+            onPressed: () {
+              PermissionHandler().requestPermissions(
+                  [PermissionGroup.locationWhenInUse]).then((ignored) {
+                _fetchPermissionStatus();
+              });
+            },
+          ),
+          SizedBox(height: 16),
+          RaisedButton(
+            child: Text('Open App Settings'),
+            onPressed: () {
+              PermissionHandler().openAppSettings().then((opened) {
+                //
+              });
+            },
+          )
+        ],
       ),
     );
+  }
+
+  void _fetchPermissionStatus() {
+    PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.locationWhenInUse)
+        .then((status) {
+      if (mounted) {
+        setState(() => _hasPermissions = status == PermissionStatus.granted);
+      }
+    });
   }
 }
