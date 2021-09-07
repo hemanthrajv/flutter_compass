0591cbd115de19ff41d09d8510d2ab76da462299
Joseph Crowell <joseph.w.crowell@gmail.com>
2021-09-08 09:46:25 +1000

    upgrade example app to Android API v2.0+

--- example/android/app/src/main/java/com/hemanthraj/fluttercompassexample/MainActivity.java
+++ example/android/app/src/main/java/com/hemanthraj/fluttercompassexample/MainActivity.java
@@ -1,15 +1,5 @@
 package com.hemanthraj.fluttercompassexample;
 
-import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        GeneratedPluginRegistrant.registerWith(this);
-    }
-}
+public class MainActivity extends FlutterActivity {}
