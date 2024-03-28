93b674df16556b5acb6d2a71a373c39bd94820d2
yassine <mohamedyassine.sabri@value.com.tn>
2024-03-28 13:22:24 +0100

    fix ios issue

--- ios/Classes/FlutterCompassPlugin.m
+++ ios/Classes/FlutterCompassPlugin.m
@@ -1,5 +1,5 @@
 #import "FlutterCompassPlugin.h"
-#import <flutter_compass/flutter_compass-Swift.h>
+#import <flutter_compass_v2/flutter_compass_v2-Swift.h>
 
 @implementation FlutterCompassPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
